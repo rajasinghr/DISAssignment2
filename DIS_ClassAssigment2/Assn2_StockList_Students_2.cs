3b6147a5f2c150c4af9bfa4ec0f4ed16afff7193
vigneshwarsripad <vigneshwarsripad@gmail.com>
2018-10-01 20:02:43 -0400

    Added comments for Student 2 file

--- DIS_ClassAssigment2/Assn2_StockList_Students_2.cs
+++ DIS_ClassAssigment2/Assn2_StockList_Students_2.cs
@@ -4,65 +4,72 @@ using System.Text;
 
 namespace Assignment_2
 {
-  public partial class StockList
-  {
-    //param   (StockList)listToMerge : second list to be merged 
-    //summary      : merge two different list into a single result list
-    //return       : merged list
-    //return type  : StockList
-    public StockList MergeList(StockList listToMerge)
+    public partial class StockList
     {
+        //param   (StockList)listToMerge : second list to be merged 
+        //summary      : merge two different list into a single result list
+        //return       : merged list
+        //return type  : StockList
+        public StockList MergeList(StockList listToMerge)
+        {
             StockList resultList = new StockList();
+            //Checking if list is empty
             if (this.IsEmpty())
             {
-                Console.WriteLine("List is empty");
+                Console.WriteLine("There is no stock for base client");
 
             }
-            else { 
-            StockNode current1 = this.head;
-
-            
-            while (current1 != null)
+            else
             {
-                resultList.AddStock(current1.StockHolding);
-                current1 = current1.Next;
+                StockNode current1 = this.head;
+
+                //Adding the stock value to the list
+                while (current1 != null)
+                {
+                    resultList.AddStock(current1.StockHolding);
+                    current1 = current1.Next;
+                }
             }
-            }
+            //Checking if list is empty
             if (this.IsEmpty())
             {
-                Console.WriteLine("List is empty");
+                Console.WriteLine("There is no stock for client to be merged");
 
             }
-            else { 
-            StockNode current2 = listToMerge.head;
-            
-            while (current2 != null)
+            else
             {
-                resultList.AddStock(current2.StockHolding);
-                current2 = current2.Next;
-            }
+                StockNode current2 = listToMerge.head;
+                //Adding the stock value to the list
+                while (current2 != null)
+                {
+                    resultList.AddStock(current2.StockHolding);
+                    current2 = current2.Next;
+                }
             }
             return resultList;
-    }
+        }
 
-    //param        : NA
-    //summary      : finds the stock with most number of holdings
-    //return       : stock with most shares
-    //return type  : Stock
-    public Stock MostShares()
-    {
+        //param        : NA
+        //summary      : finds the stock with most number of holdings
+        //return       : stock with most shares
+        //return type  : Stock
+        public Stock MostShares()
+        {
             Stock mostShareStock = null;
-            StockNode current = this.head;
+
+            //Checking if list is empty
             if (this.IsEmpty())
             {
                 Console.WriteLine("List is empty");
 
             }
+            StockNode current = this.head;
             mostShareStock = current.StockHolding;
             current = current.Next;
+            //Finding stock with most shares by storing the value in the variable mostShareStock
             while (current != null)
             {
-                if (current.StockHolding.Holdings> mostShareStock.Holdings )
+                if (current.StockHolding.Holdings > mostShareStock.Holdings)
                 {
 
                     mostShareStock = current.StockHolding;
@@ -71,23 +78,24 @@ namespace Assignment_2
             }
             Console.WriteLine(mostShareStock);
             return mostShareStock;
-    }
+        }
 
-    //param        : NA
-    //summary      : finds the number of nodes present in the list
-    //return       : length of list
-    //return type  : int
-    public int Length()
-    {
-      int length = 0;
-
+        //param        : NA
+        //summary      : finds the number of nodes present in the list
+        //return       : length of list
+        //return type  : int
+        public int Length()
+        {
+            int length = 0;
+            //Checking if list is empty
             if (this.IsEmpty())
             {
                 Console.WriteLine("List is empty");
 
             }
-            
+
             StockNode current = this.head;
+            //Finding length by incrementing length value until there are no more items in the list
             while (current != null)
             {
                 length++;
@@ -95,6 +103,6 @@ namespace Assignment_2
             }
 
             return length;
+        }
     }
-  }
 }
